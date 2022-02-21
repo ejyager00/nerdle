7c7024fd2104f3a20ed682035a9a56c7d643eda9
Eric Yager <ejyager00@gmail.com>
2022-02-21 07:26:58 -0600

    bug fixes

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/ejyager00/nerdle
 
 go 1.17
 
-require github.com/ejyager00/nerdle/nerdlegame v0.0.0-20220219213611-bc037a405d3d
+require github.com/ejyager00/nerdle/nerdlegame v0.0.0-20220219221419-5edb63788d78
 
-require github.com/ejyager00/nerdle/evaluate v0.0.0-20220219213611-bc037a405d3d // indirect
+require github.com/ejyager00/nerdle/evaluate v0.0.0-20220221020858-85a4001636f3 // indirect
--- nerdle.go
+++ nerdle.go
@@ -5,15 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ejyager00/nerdle/nerdlegame"
 )
-
-const PUZZLE_LENGTH int = 8
-const MAX_GUESSES int = 6
-const LEADING_ZEROS_ALLOWED bool = false
-const NEGATIVE_ZERO_ALLOWED bool = false
-const ZERO_REMOVAL_RATE float32 = 1
 
 type Game struct {
 	puzzle        string
@@ -41,12 +36,8 @@ var games map[int64]Game
 var currentKey int64 = 0
 
 func main() {
-	for i := 0; i < 20; i++ {
-		p := nerdlegame.WeightedRandomPuzzle(PUZZLE_LENGTH, LEADING_ZEROS_ALLOWED, NEGATIVE_ZERO_ALLOWED, ZERO_REMOVAL_RATE)
-		fmt.Println(p)
-		fmt.Println(nerdlegame.MakeGuess("11+17=28", "19+-7=12", LEADING_ZEROS_ALLOWED, NEGATIVE_ZERO_ALLOWED))
-		fmt.Println(nerdlegame.MakeGuess("11+17=28", "19+-7=12", LEADING_ZEROS_ALLOWED, NEGATIVE_ZERO_ALLOWED))
-	}
+	games = make(map[int64]Game)
+	handleRequests()
 }
 
 func handleRequests() {
@@ -57,14 +48,12 @@ func handleRequests() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, "Hello, world!")
-	fmt.Println("Endpoint Hit: homePage")
+	index, _ := os.ReadFile("index.html")
+	fmt.Fprint(w, string(index))
 }
 
 func newPuzzle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	decoder := json.NewDecoder(r.Body)
 	var req GameStart
 	err := decoder.Decode(&req)
@@ -76,14 +65,12 @@ func newPuzzle(w http.ResponseWriter, r *http.Request) {
 		key := currentKey
 		currentKey++
 		games[key] = game
-		fmt.Printf("{\"key\":%d}\n", key)
 		fmt.Fprintf(w, "{\"key\":%d}", key)
 	}
 }
 
 func guessHandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	decoder := json.NewDecoder(r.Body)
 	var req Guess
 	err := decoder.Decode(&req)
@@ -108,11 +95,11 @@ func guessHandle(w http.ResponseWriter, r *http.Request) {
 		}
 		comparison_string += "]"
 		if won {
-			fmt.Fprintf(w, "{\"comparison\"%s:,\"validguess\":true,\"won\":true,\"loss\":false,\"guesses\":%d}", comparison_string, game.guesses)
+			fmt.Fprintf(w, "{\"comparison\":%s,\"validguess\":true,\"won\":true,\"loss\":false,\"guesses\":%d}", comparison_string, game.guesses)
 		} else if game.guesses == game.maxguesses {
 			fmt.Fprintf(w, "{\"comparison\":%s,\"validguess\":true,\"won\":false,\"loss\":true,\"guesses\":%d,\"solution\":%#v}", comparison_string, game.guesses, game.puzzle)
 		} else {
-			fmt.Fprintf(w, "{\"comparison\":%s,\"validguess\":true,\"won\":false,\"loss\":false,\"guesses\":%d}}", comparison_string, game.guesses)
+			fmt.Fprintf(w, "{\"comparison\":%s,\"validguess\":true,\"won\":false,\"loss\":false,\"guesses\":%d}", comparison_string, game.guesses)
 		}
 		games[req.Key] = game
 	}
